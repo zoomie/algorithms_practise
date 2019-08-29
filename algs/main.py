984f49a3f840bf184143ef1210bb3dd6d5b7ec1d
Andrew Arderne <andrew.arderne@navenio.com>
2019-08-29 23:58:21 +0100

    started tests

--- algs/main.py
+++ algs/main.py
@@ -4,16 +4,16 @@
 
 from typing import List
 
-str1 = 'man'
-str2 = 'mun'
 
-
-def change(str1: str, str2: str) -> List[str]:
+def edit_string(str1: str, str2: str) -> List[str]:
     operations: List[str] = []
+
     for char1, char2 in zip(str1, str2):
         if char1 != char2:
             operations.append("replace")
     return operations
 
 
-print(change(str1, str2))
+str1 = 'man'
+str2 = 'mun'
+print(edit_string(str1, str2))
